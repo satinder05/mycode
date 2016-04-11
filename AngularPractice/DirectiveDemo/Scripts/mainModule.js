4e53cee52c34e0879f74df46b7edce0826020554
Satinder <satinder.s@makeitmine.com.au>
2016-04-11 12:56:01 +1000

    first commit

---
+++ AngularPractice/DirectiveDemo/Scripts/mainModule.js
@@ -0,0 +1,76 @@
+﻿var mainModule = angular.module('mainModule', []);
+
+mainModule.controller('mainController', ['$scope', '$window', function ($scope, $window) {
+
+    $scope.John = {
+        name: 'John',
+        address: '58 collins st, Melbourne'
+    };
+
+    $scope.Jack = {
+        name: 'Jack',
+        address: 'Jacks Address'
+    };
+
+    $scope.name = 'Tobias';
+
+    $scope.Clicked = function () {
+        
+        $window.alert("Click Event");
+    };
+
+    $scope.Text = "abc";
+
+}]);
+
+mainModule.directive('myCustomer', function () {
+
+    return {
+        restrict: 'AE',
+        scope: {
+            customerInfo : '=infoCustomer'
+        },
+        //transclude : true,
+        templateUrl : 'MyTemplate.html'
+    };
+
+});
+
+mainModule.directive('myTest', function () {
+
+    return {
+        restrict: 'E',
+        //transclude : true,
+        template : '<div>Name : {{Jack.name}} <br />Address : {{Jack.address}}</div>'
+    };
+
+});
+
+mainModule.directive('myDialog', function () {
+
+    return {
+        restrict: 'E',
+        transclude: true,
+        template: '<div class="alert" ng-transclude></div>'
+    };
+
+});
+
+mainModule.directive('myButton', function () {
+
+    return {
+        restrict: 'E',
+        scope: {
+            clickFunction : '&'
+        },
+        template: '<input type="button", value="Click Directive" ng-click="clickFunction()" />',
+
+        link: function ($scope) {
+            
+            $scope.myfunction = function () {
+                alert("Click From Directive");
+            };
+        }
+    };
+
+});
---
+++ AngularPractice/Scripts/myApp.js
@@ -0,0 +1,57 @@
+﻿var mainModule = angular.module('mainModule', ['factoryModule', 'serModule', 'provModule']);
+
+mainModule.config(function (greetProvider) {
+    greetProvider.setName('From Provider');
+});
+
+mainModule.controller('mainModuleController', ['$scope', 'greetFactory', 'greetService', 'greet', function ($scope, greetFactory, greetService, greet) {
+
+    $scope.greet = "Hello World";
+    $scope.greetFromFactory = greetFactory.SayHello();
+    $scope.Servicegreeting = greetService.SayHello();
+    $scope.greetProvider = greet.sayHello();
+
+}]);
+
+var factoryModule = angular.module('factoryModule', []);
+
+factoryModule.factory('greetFactory', function () {
+    return { 
+        SayHello: function(){
+            return "Hello From Factory"
+        }
+    };
+});
+
+
+var serModule = angular.module('serModule', []);
+
+serModule.service('greetService', function () {
+    
+    this.SayHello = function () {
+        return "Hello from Service";
+    }
+});
+
+var provModule = angular.module('provModule', []);
+
+provModule.provider('greet', function () {
+    
+    this.name = 'Default';
+
+    this.setName = function (name) {
+        this.name = name;
+    };
+
+
+    this.$get = function() {
+        var name = this.name;
+
+        return {
+            sayHello: function () {
+                return "Hello " + name;
+            }
+        }
+    }
+
+});
